b5c6672c3fa001166b204442675f29672a48dd4e
BVerstaen <bverstaen04@gmail.com>
2025-09-10 10:04:14 +0200

    Initial commit

---
+++ MatricesUnitTest/MatrixInt.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Maths_Matrices.Tests
+{
+    public struct MatrixInt
+    {
+        private int[,] matrice;
+
+        public MatrixInt(int columns, int lines)
+        {
+            matrice = new int[columns, lines];
+        }
+
+        public MatrixInt(int[,] matrice)
+        {
+            this.matrice = matrice;
+        }
+
+        public MatrixInt(MatrixInt matriceToCopy)
+        {
+            matrice = new int[0,0];
+            
+            Array.Copy(matriceToCopy, 0, matrice, 0,);
+        }
+
+        public int NbLines => matrice.GetLength(0);
+        public int NbColumns => matrice.GetLength(1);
+        public int[,] ToArray2D() => matrice;
+        public int this[int i, int i1]
+        {
+            get => matrice[i, i1];
+            set => matrice[i, i1] = value;
+        }
+    }
+}
