18950b6a63f9cb9e8f176a0b4d0c82fb8266dc87
BVerstaen <bverstaen04@gmail.com>
2025-09-11 11:10:51 +0200

    Half working Test 10

--- MatricesUnitTest/MatrixRowReductionAlgorithm.cs
+++ MatricesUnitTest/MatrixRowReductionAlgorithm.cs
@@ -7,40 +7,45 @@ namespace Maths_Matrices.Tests
         public static (MatrixFloat, MatrixFloat) Apply(MatrixFloat m1, MatrixFloat m2)
         {
             MatrixFloat augmentedMatrix = MatrixFloat.GenerateAugmentedMatrix(m1, m2);
-            
-            for (int j = 0; j < augmentedMatrix.NbColumns; j++)
-            {
-                for (int i = 0; i < augmentedMatrix.NbLines; i++)
-                {
-                    //Find highest number
-                    float highestNumber = augmentedMatrix[i, j];
-                    int highestLine = i;
-                    for (int k = i; k < augmentedMatrix.NbLines; k++)
-                    {
-                        if (augmentedMatrix[k, j] > highestNumber)
-                        {
-                            highestNumber = augmentedMatrix[k, j];
-                            highestLine = k;
-                        }
-                    }
-                    //Swap lines if necessary
-                    if (highestLine != i)
-                        MatrixElementaryOperations.SwapLines(augmentedMatrix, highestLine, i);
-                    
-                    //Multiply line
-                    float lineScalarMultiplier = 1.0f / augmentedMatrix[i, j];
-                    MatrixElementaryOperations.MultiplyLine(augmentedMatrix, i, lineScalarMultiplier);
 
-                    //Add other lines
-                    for (int r = 0; r < augmentedMatrix.NbLines; r++)
+            int i = 0;
+            for (int j = 0; j < m1.NbColumns; j++)
+            {
+                
+                //Find highest number
+                float highestNumber = augmentedMatrix[i, j];
+                int highestLine = i;
+                for (int k = i; k < augmentedMatrix.NbLines; k++)
+                {
+                    if (augmentedMatrix[k, j] > highestNumber && augmentedMatrix[k, j] != 0)
                     {
-                        if (r != i)
-                            continue;
-                        
-                        float factor = Math.Abs(-augmentedMatrix[r, j]);
-                        MatrixElementaryOperations.AddLineToAnother(augmentedMatrix, i, r, factor);
+                        highestNumber = augmentedMatrix[k, j];
+                        highestLine = k;
                     }
                 }
+                //Check non-null
+                if(highestLine == 0)
+                    continue;
+                
+                //Swap lines if necessary
+                if (highestLine != i)
+                    MatrixElementaryOperations.SwapLines(augmentedMatrix, highestLine, i);
+                    
+                //Multiply line
+                float lineScalarMultiplier = 1.0f / augmentedMatrix[i, j];
+                MatrixElementaryOperations.MultiplyLine(augmentedMatrix, i, lineScalarMultiplier);
+
+                //Add other lines
+                for (int r = 0; r < augmentedMatrix.NbLines; r++)
+                {
+                    if (r == i)
+                        continue;
+                        
+                    float factor = -augmentedMatrix[r, j];
+                    MatrixElementaryOperations.AddLineToAnother(augmentedMatrix, i, r, factor);
+                }
+
+                i++;
             }
 
             return augmentedMatrix.Split(augmentedMatrix.NbColumns - 2);
--- MatricesUnitTest/Tests10_RowReduction.cs
+++ MatricesUnitTest/Tests10_RowReduction.cs
@@ -43,42 +43,42 @@ namespace Maths_Matrices.Tests
             GlobalSettings.DefaultFloatingPointTolerance = 0.0d;
         }
         
-        // [Test]
-        // public void TestApplyRowReduction_Exercise()
-        // {
-        //     MatrixFloat m1 = new MatrixFloat(new[,]
-        //     {
-        //         { 2f, 1f, 3f },
-        //         { 0f, 1f, -1f },
-        //         { 1f, 3f, -1f }
-        //     });
-        //
-        //     MatrixFloat m2 = new MatrixFloat(new[,]
-        //     {
-        //         { 0f },
-        //         { 0f },
-        //         { 0f }
-        //     });
-        //
-        //     //This method use deconstruction tuple system
-        //     //More information here =>
-        //     //https://docs.microsoft.com/fr-fr/dotnet/csharp/fundamentals/functional/deconstruct
-        //     (m1, m2) = MatrixRowReductionAlgorithm.Apply(m1, m2);
-        //     GlobalSettings.DefaultFloatingPointTolerance = 0.001f;
-        //     Assert.AreEqual(new[,]
-        //     {
-        //         { 1f, 0f, 2f },
-        //         { 0f, 1f, -1f },
-        //         { 0f, 0f, 0f }
-        //     }, m1.ToArray2D());
-        //
-        //     Assert.AreEqual(new[,]
-        //     {
-        //         { 0f },
-        //         { 0f },
-        //         { 0f }
-        //     }, m2.ToArray2D());
-        //     GlobalSettings.DefaultFloatingPointTolerance = 0.0d;
-        // }
+        [Test]
+        public void TestApplyRowReduction_Exercise()
+        {
+            MatrixFloat m1 = new MatrixFloat(new[,]
+            {
+                { 2f, 1f, 3f },
+                { 0f, 1f, -1f },
+                { 1f, 3f, -1f }
+            });
+        
+            MatrixFloat m2 = new MatrixFloat(new[,]
+            {
+                { 0f },
+                { 0f },
+                { 0f }
+            });
+        
+            //This method use deconstruction tuple system
+            //More information here =>
+            //https://docs.microsoft.com/fr-fr/dotnet/csharp/fundamentals/functional/deconstruct
+            (m1, m2) = MatrixRowReductionAlgorithm.Apply(m1, m2);
+            GlobalSettings.DefaultFloatingPointTolerance = 0.001f;
+            Assert.AreEqual(new[,]
+            {
+                { 1f, 0f, 2f },
+                { 0f, 1f, -1f },
+                { 0f, 0f, 0f }
+            }, m1.ToArray2D());
+        
+            Assert.AreEqual(new[,]
+            {
+                { 0f },
+                { 0f },
+                { 0f }
+            }, m2.ToArray2D());
+            GlobalSettings.DefaultFloatingPointTolerance = 0.0d;
+        }
     }
 }
