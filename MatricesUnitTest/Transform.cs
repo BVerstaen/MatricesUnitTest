6af60f16624dc78e39ca3be3acddf095b72271a2
BVerstaen <bverstaen04@gmail.com>
2025-10-01 19:34:09 +0200

    [ADD] Tests 17 + Add 18

---
+++ MatricesUnitTest/Transform.cs
@@ -0,0 +1,26 @@
+﻿namespace Maths_Matrices.Tests
+{
+    public class Transform
+    {
+        public Vector3 LocalPosition;
+
+        public MatrixFloat LocalTranslationMatrix
+        {
+            get
+            {
+                MatrixFloat matrix = MatrixFloat.Identity(4);
+                //Set position manually
+                matrix[0, 3] = LocalPosition.x;
+                matrix[1, 3] = LocalPosition.y;
+                matrix[2, 3] = LocalPosition.z;
+                
+                return matrix;
+            }
+        }
+
+        public Transform()
+        {
+            LocalPosition = new Vector3(0f, 0f, 0f);
+        }
+    }
+}
---
+++ MatricesUnitTest/Vector3.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Maths_Matrices.Tests
+{
+    public struct Vector3
+    {
+        public float x, y, z;
+
+        public Vector3(float x, float y, float z)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+        
+        public Vector3(MatrixFloat matrix)
+        {
+            if(matrix.NbColumns != 1 || matrix.NbLines != 4)
+                throw new ArgumentException("Matrix must be 4x1");
+            
+            this.x = matrix[0,0];
+            this.y = matrix[1,0];
+            this.z = matrix[2,0];
+        }
+    }
+}
